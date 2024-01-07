b8a6cd51cdf3a3abec6c7c9a19b93cff6fa1d6a4
freelancersifat627e <freelancersifat627@gmail.comm>
2024-01-07 12:39:33 +0600

    hide firebase config

--- src/utilities/firebase/firebase.config.js
+++ src/utilities/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('environment variable' , import.meta.env.VITE_PASS )
 const firebaseConfig = {
-  apiKey: "AIzaSyApT_R7zRIEavG2i9RlF6j-M_zPQFF7_-o",
-  authDomain: "chef-recipe-hunter-clien-65b2d.firebaseapp.com",
-  projectId: "chef-recipe-hunter-clien-65b2d",
-  storageBucket: "chef-recipe-hunter-clien-65b2d.appspot.com",
-  messagingSenderId: "1030767412656",
-  appId: "1:1030767412656:web:fdddbd458ba2744fc94275"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
